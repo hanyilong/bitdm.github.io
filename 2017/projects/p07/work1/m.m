c9849c3f70647acc2e9c1509c837dd9566dd5f90
hanyilong <hyl-7908@163.com>
2017-04-12 12:06:37 +0800

    init

--- 2017/projects/p07/work1/m.m
+++ 2017/projects/p07/work1/m.m
@@ -3,5 +3,6 @@ if size(td)<1
     testData = readFile('data/horse-colic.test')
     td = [trainData;testData]
 end
-fldArr = scatterFld(td,7)
-fldFre = scatterFrequency(td,7,fldArr)
+scatterFldArr = scatterFld(td,7)
+scatterFldFre = scatterFrequency(td,7,fldArr)
+numberFldArr = numberFldProcess(td,4)
--- 2017/projects/p07/work1/numberFldProcess.m
+++ 2017/projects/p07/work1/numberFldProcess.m
@@ -1,24 +1,24 @@
 function Z = numberFldProcess(data,idx)
     arr=zeros(0)
     orderArr=zeros(0)
-    max = 0
-    min = 0
     lost = 0
     middle = 0
-    q1 = 0
-    q2 = 0
-    q3 = 0
-    
     for  i=1:length(data)
         d0 = data(i,idx)
-        if ismember(d0,e)
-            continue
-        end
         if isnan(d0)
             continue
         end
-
+        arr(length(arr)+1) = d0
     end
-    Z = e
+    orderArr = sort(arr)
+    Z.arr = arr
+    Z.orderArr = orderArr
+    Z.max = orderArr(length(orderArr))
+    Z.min = orderArr(1)
+    Z.lost = length(data) - length(orderArr)
+    Z.middle = orderArr(round(length(orderArr)/2))
+    Z.q1 = orderArr(round((length(orderArr)+1)/4))
+    Z.q2 = orderArr(round(2*(length(orderArr)+1)/4))
+    Z.q3 = orderArr(round(3*(length(orderArr)+1)/4))
     
         
